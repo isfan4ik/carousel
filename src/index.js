346a7f28ffcffa816f2675104a38f877effc4664
isfan4ik <isfandiyorprogrammer@gmail.com>
2024-02-09 13:29:52 +0500

    caroulse

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ImageSlider from './Components/ImageSlider/ImageSlider';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ImageSlider />
+);
